7621e14b641c78493c44c4237395518ea4c899a6
malte <malt0533@stud.kea.dk>
2022-04-06 16:15:04 +0200

    End of day, 06.03.22

--- js/databse.js
+++ js/databse.js
@@ -35,6 +35,35 @@ export async function getDataDB(url, apiKey) {
 		},
 	});
 	const json = await fetchedData.json();
+}
 
-	console.log(json);
+export async function getRelevantData(queryUrl) {
+	const url = `https://advice-95b4.restdb.io/rest/url-db?q={"url":"https://${queryUrl}/"}`;
+	const apiKey = "624bef0c67937c128d7c94e3";
+
+	const fetchedData = await fetch(url, {
+		async: true,
+		crossDomain: true,
+		url: url,
+		method: "get",
+		headers: {
+			"content-type": "application/json",
+			"x-apikey": apiKey,
+			"cache-control": "no-cache",
+		},
+	});
+
+	const json = await fetchedData.json();
+
+	if (json.length !== 0) {
+		console.log("DEn her URL findes!");
+		console.log(json);
+		const data = { ...json[0] };
+		console.log(data);
+		return data;
+	} else {
+		console.log("Den her url eksisterer ikke");
+
+		return {};
+	}
 }
--- main.js
+++ main.js
@@ -1,15 +1,50 @@
 "use strict";
 
 import { getPageInsight, getCarbonMetrics } from "./js/api";
-import { postHandler, getDataDB } from "./js/postDB";
+import { postHandler, getDataDB, getRelevantData } from "./js/databse";
 async function init() {
 	//getDataDB(url, apiKey);
 
 	// getPageInsight("designbymagnus.dk");
 
 	// POST Metrics
-	const metrics = await getCarbonMetrics("designbymagnus.dk", "Porn");
-	postHandler(metrics);
+	// const metrics = await getCarbonMetrics("designbymagnus.dk", "Porn");
+	// postHandler(metrics);
+
+	// getRelevantData("aaaaa.dk/");
+
+	const url = "designbymagnus.dk";
+
+	//? First step
+	//todo Check for input
+
+	//todo When generate is clicked, get input
+
+	//todo Check if input exists in database
+	const data = await getRelevantData(url);
+	console.log("This is the returned data " + data);
+
+	//todo If yes, show relevant data
+
+	//todo If no, GET website carbon data and set flag to false
+	//todo Show data
+
+	//? Second step
+	//todo Check if generate further is clicked
+
+	//todo If flag false
+
+	//todo Get page insight
+
+	//todo merge carbon + page insight
+
+	//todo POST All data
+
+	//todo show LOADING SCREEN
+
+	//todo show result
+
+	//todo click share result
 }
 
 init();
